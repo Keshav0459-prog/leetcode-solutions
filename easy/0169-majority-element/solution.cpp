981a0adab3cf6f4de188afa686a13221952f8001
Keshav Kumar Sharma <kk8610988@gmail.com>
2026-08-29 19:52:19 +0530

    ✅ Majority Element | Easy | C++ | Runtime: 3 ms (28.71%) | Memory: 28 MB | 2026-08-29

--- easy/0169-majority-element/solution.cpp
+++ easy/0169-majority-element/solution.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        int a = 0;
-        for(int b : nums){
-            a ^= b;
+    int majorityElement(vector<int>& nums) {
+        int a=0;
+        int count=0;
+        for(int num: nums){
+            if(count ==0){
+                a = num;
+            }
+            if(num == a){
+                count ++;
+            }
+            else {
+                count --;
+            }
         }
         return a;
     }
