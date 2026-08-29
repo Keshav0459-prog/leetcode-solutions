8c79db30aad193976eb9a60a1915d9715f04c066
Keshav Kumar Sharma <kk8610988@gmail.com>
2026-08-29 19:35:54 +0530

    ✅ Single Number | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 20.7 MB | 2026-08-29

--- easy/0136-single-number/solution.cpp
+++ easy/0136-single-number/solution.cpp
@@ -1,10 +1,10 @@
-class Solution {
+class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        int a = 0;
-        for(int b : nums){
-            a = a ^ b;
-        }
-        return a;
-    }
+    int singleNumber(vector<int>& nums) {
+        int a = 0;
+        for(int b : nums){
+            a = a ^ b;
+        }
+        return a;
+    }
 };
