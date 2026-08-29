5e414370bfa887a9dc2faa216e9099818e81d63a
Keshav Kumar Sharma <kk8610988@gmail.com>
2026-08-29 20:11:10 +0530

    ✅ Maximum Subarray | Medium | C++ | Runtime: 1 ms (25.21%) | Memory: 71.8 MB | 2026-08-29

--- medium/0053-maximum-subarray/solution.cpp
+++ medium/0053-maximum-subarray/solution.cpp
@@ -1,14 +1,14 @@
-class Solution {
+class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-       int currSum =0; int maxSum= INT_MIN;
-       for(int val: nums){
-        currSum += val;
-        maxSum = max(currSum, maxSum);
-        if(currSum < 0){
-            currSum = 0;
-        }
-       }
-       return maxSum;
-    }
+    int maxSubArray(vector<int>& nums) {
+       int currSum =0; int maxSum= INT_MIN;
+       for(int val: nums){
+        currSum += val;
+        maxSum = max(currSum, maxSum);
+        if(currSum < 0){
+            currSum = 0;
+        }
+       }
+       return maxSum;
+    }
 };
