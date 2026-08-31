f3e33bf40caeac27fdc905ca1f89ffc31f7c142b
Keshav Kumar Sharma <kk8610988@gmail.com>
2026-08-31 09:36:32 +0530

    ✅ Majority Element II | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-08-31

---
+++ medium/0229-majority-element-ii/solution.cpp
@@ -0,0 +1,16 @@
+        int freq = 0;
+        for(int el: nums){
+            if(val == el){
+                freq++;
+            }
+        }
+        if(freq> n/3){
+            result.emplace_back(val);
+        }
+      }
+      for(int val: nums){
+      vector<int> result;
+      int n = nums.size();
+    vector<int> majorityElement(vector<int>& nums) {
+public:
+class Solution {
