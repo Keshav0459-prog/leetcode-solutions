d42fca4be18ee9130e4302f285a175fb81636ef6
Keshav Kumar Sharma <kk8610988@gmail.com>
2026-09-08 00:30:35 +0530

    ✅ Fibonacci Number | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 7.9 MB | 2026-09-07

--- easy/0509-fibonacci-number/solution.cpp
+++ easy/0509-fibonacci-number/solution.cpp
@@ -1,14 +1,14 @@
-class Solution {
+class Solution {
 public:
-    int fib(int n) {
-        int a = 0;
-        int b = 1;
-        int nextterm = 0;
-        for(int i =0; i<n; i++){
-            nextterm = a+b;
-            a=b;
-            b=nextterm;
-        }
-        return a;
-    }
+    int fib(int n) {
+        int a = 0;
+        int b = 1;
+        int nextterm = 0;
+        for(int i =0; i<n; i++){
+            nextterm = a+b;
+            a=b;
+            b=nextterm;
+        }
+        return a;
+    }
 };
