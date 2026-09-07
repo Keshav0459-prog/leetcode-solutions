136ffc5c6b8a716b3c30ec6f2cd082705163b003
Keshav Kumar Sharma <kk8610988@gmail.com>
2026-09-08 00:54:47 +0530

    ✅ Subtract the Product and Sum of Digits of an Integer | Easy | C++ | Runtime: 0 ms | Memory: 7.7 MB | 2026-09-07

---
+++ easy/1281-subtract-the-product-and-sum-of-digits-of-an-integer/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+       int multiply = 1;
+       int add = 0;
+       
+       while(n>0){
+        int rem = n % 10;
+        multiply *= rem;
+        add+= add;
+        n/=10;
+       }
+       return multiply - add; 
+    }
+};
