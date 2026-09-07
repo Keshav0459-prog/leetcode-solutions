2efce42a3d7198a789905d641e923c9119fdbbc5
Keshav Kumar Sharma <kk8610988@gmail.com>
2026-09-08 01:30:07 +0530

    ✅ Count Primes | Medium | C++ | Runtime: 0 ms | Memory: 7.8 MB | 2026-09-07

--- medium/0204-count-primes/solution.cpp
+++ medium/0204-count-primes/solution.cpp
@@ -1,12 +1,17 @@
-class Solution {
-public:
-    int countPrimes(int n) {
-        if(n<=1) return false;
-        for(int i=2; i*i <=n; i++){
-            if(n%i==0){
-                return false;
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) {
+                count++; 
+                for (long long j = 1LL * i * i; j < n; j += i) {
+                    prime[j] = false;
+                }
             }
         }
+        
+        return count;
+        
     }
-        return true;
-};
+        
+        int count = 0;
+        vector<bool> prime(n, true);
+    int countPrimes(int n) {
+public:
