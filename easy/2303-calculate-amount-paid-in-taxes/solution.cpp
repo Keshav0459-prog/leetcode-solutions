255f012df6586c823d0b7752e904446a25f4b170
Keshav Kumar Sharma <kk8610988@gmail.com>
2026-09-08 00:19:58 +0530

    ✅ Calculate Amount Paid in Taxes | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 17.5 MB | 2026-09-07

--- easy/2303-calculate-amount-paid-in-taxes/solution.cpp
+++ easy/2303-calculate-amount-paid-in-taxes/solution.cpp
@@ -1,16 +1,20 @@
-            int upper = b[0];
-            int percent = b[1];
-            int tax = min(income,upper) - pre;
-            if(tax > 0){
-                total += (tax * percent)/ 100.0;
-            }
-            pre = upper;
-        for(const auto& b : brackets){
-        int pre=0;
-        double total = 0.0;
-            if (income <= upper){
-                break;
-            }
-    double calculateTax(vector<vector<int>>& brackets, int income) {
+class Solution {
 public:
-class Solution {
+    double calculateTax(vector<vector<int>>& brackets, int income) {
+        double total = 0.0;
+        int pre=0;
+        for(const auto& b : brackets){
+            int upper = b[0];
+            int percent = b[1];
+            int tax = min(income,upper) - pre;
+            if(tax > 0){
+                total += (tax * percent)/ 100.0;
+            }
+            pre = upper;
+            if (income <= upper){
+                break;
+            }
+        } 
+        return total;
+    }
+};
