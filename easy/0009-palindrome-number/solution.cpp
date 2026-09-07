f74b156dbf8d97fe15195b155b3eb32af6d8306b
Keshav Kumar Sharma <kk8610988@gmail.com>
2026-09-08 01:14:25 +0530

    ✅ Palindrome Number | Easy | C++ | Runtime: 0 ms | Memory: 7.8 MB | 2026-09-07

--- easy/0009-palindrome-number/solution.cpp
+++ easy/0009-palindrome-number/solution.cpp
@@ -1,16 +1,11 @@
-class Solution {
 public:
     bool isPalindrome(int x) {
         int reverse = 0;
         while(x!=0){
             int rem = x % 10;
             reverse = (reverse*10) + rem;
-        } return reverse;
             x/=10;
-        if(reverse == x){
-            return true;
-        } else {
-            return false;
-        }
+        } return reverse == x;
     }
 };
+class Solution {
