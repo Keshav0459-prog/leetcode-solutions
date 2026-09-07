525c31e06a69f4e4e2d53802198ae09ef9bcd4f0
Keshav Kumar Sharma <kk8610988@gmail.com>
2026-09-08 00:11:25 +0530

    ✅ Fizz Buzz | Easy | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-09-07

---
+++ easy/0412-fizz-buzz/solution.cpp
@@ -0,0 +1,16 @@
+            else if(i%3==0){
+                answer.push_back("Fizz");
+            }
+            else if(i%5==0){
+                answer.push_back("Buzz");
+            }
+            else{
+                answer.push_back(to_string(i));
+            }
+                answer.push_back("FizzBuzz");
+            if(i%3==0 && i%5==0){
+        for(int i=0; i<n; i++){
+        vector<string> answer;
+    vector<string> fizzBuzz(int n) {
+public:
+class Solution {
