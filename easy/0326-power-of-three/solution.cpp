17653823601ebee6acad1e927bbfa89ef0497a1a
Keshav Kumar Sharma <kk8610988@gmail.com>
2026-09-08 00:46:55 +0530

    ✅ Power of Three | Easy | C++ | Runtime: 0 ms | Memory: 7.7 MB | 2026-09-07

--- easy/0326-power-of-three/solution.cpp
+++ easy/0326-power-of-three/solution.cpp
@@ -1,12 +1,12 @@
-class Solution {
+class Solution {
 public:
-    bool isPowerOfThree(int n) {
-        if(n<=0){
-            return false;
-        }
-        while(n%3==0){
-            n/=3;
-        }
-    }
-        return true;
+    bool isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n%3==0){
+            n/=3;
+        }
+        return true;
+    }
 };
